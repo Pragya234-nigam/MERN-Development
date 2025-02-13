cbb8eeccf713d90353b602104f7b894e210443d4
Pragya Nigam <pragyanigam234@gmail.com>
2025-02-13 18:26:09 +0530

    push

--- Section2/frontend/src/app/login/page.jsx
+++ Section2/frontend/src/app/login/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import classes from './login.module.css';
 
 const Login = () => {
   return (
     <div>
         <h1 className='text-center font-bold text-3xl'>Login Page</h1>
+
+        <button className='btn'>Login button</button>
+        <button className={classes.loginBtn}>Submit Button</button>
     </div>
   )
 }
--- Section2/frontend/src/app/page.jsx
+++ Section2/frontend/src/app/page.jsx
@@ -1,9 +1,19 @@
+import Button from '@/components/Button';
 import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-center font-bold text-5xl'>Home Page</div>//element
+    <div><h1 className='text-center font-bold text-5xl'>Home Page</h1>
+    <p style={{color:'red',fontSize:30,fontWeight:'bold'}}>Using Inline</p>
+      <br />
+      <hr />
+      <input type="text" />
+      <button className='btn'>Submit</button>
+
+      <img src="/next.svg" alt="" />
+       <Button>login</Button>
+       <Button disabled={true}>signup</Button>
+    </div>
   )
 }
-
 export default Home;
