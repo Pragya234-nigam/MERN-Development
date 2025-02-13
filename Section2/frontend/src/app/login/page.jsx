c920b549a0179f10f28dc065b3d15d07ef51d818
Pragya Nigam <pragyanigam234@gmail.com>
2025-02-13 18:25:52 +0530

    push

--- Section2/frontend/src/app/login/page.jsx
+++ Section2/frontend/src/app/login/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-        <h1 className='text-center text-3xl'>Login Page</h1>
+        <h1 className='text-center font-bold text-3xl'>Login Page</h1>
     </div>
   )
 }
